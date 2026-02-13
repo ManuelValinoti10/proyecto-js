0da24bd134a7f9a25ffa174273d63155df2a5ec8
Manuel Valinoti <manuvalinoti@gmail.com>
2026-02-13 16:08:05 -0300

    entrega1

--- main.js
+++ main.js
@@ -100,40 +100,84 @@ function saludarUsuario() {
   alert("Bienvenido " + nombre + "!!")
 }
 
-const productos = ["Iphone", "Apple Watch", "MacBook"]
+const productos = ["Iphone", "Airpods" , "Apple Watch", "MacBook"]
+const precios = [800, 130, 400, 1400]
 const carrito = []
+const carritoPrecios = []
+const productosMinuscula = []
 
-
-function mostrarProductos(productos) {
-  alert("Productos disponibles:\n" + productos.join("\n"))
+for (let i = 0; i < productos.length; i++) {
+  productosMinuscula.push(productos[i].toLowerCase())
 }
 
-function comprar(productos, carrito) {
+function mostrarProductos(productos, precios) {
+
+  let mensaje = "Productos disponibles:\n\n"
+
+  for (let i = 0; i < productos.length; i++) {
+    mensaje += productos[i] + " - $" + precios[i] + "\n"
+  }
+
+  alert(mensaje)
+}
+
+function comprar(productos, carrito, precios) {
+  let subTotal = 0
   let continuar = true
   while (continuar) {
     let productoAgregado = prompt("Agrega un producto")
     if (productoAgregado === null) {
       break
-    } else if (productos.includes(productoAgregado)) {
-      carrito.push(productoAgregado)
+    } 
+    
+    productoAgregado = productoAgregado.toLowerCase()
+
+    if (productosMinuscula.includes(productoAgregado)) {
+      let posicion = productosMinuscula.indexOf(productoAgregado)
+      let precio = precios[posicion]
+      carrito.push(productos[posicion])
+      carritoPrecios.push(precio)
+      subTotal += precio
+      alert("Producto agregado: \n" + productos[posicion] + "\nPrecio: $" + precio)
     } else {
       alert("Producto no encontrado")
     }
     continuar = confirm("Seguir agregando productos?")
   }
+  let iva = subTotal * 0.21
+  let totalConIva = subTotal + iva
+  return [subTotal, iva, totalConIva]
 }
 
-function mostrarCarrito(carrito) {
+function mostrarCarrito(carrito, carritoPrecios, total, iva, subTotal) {
   if (carrito.length) {
-    alert("Productos agregados:\n" + carrito.join("\n"))
+
+    let mensaje = "Detalle de tu compra:\n\n"
+
+    for (let i = 0; i < carrito.length; i++) {
+      mensaje += carrito[i] + " - $" + carritoPrecios[i] + "\n"
+    }
+
+    mensaje += "\n-----------------\n"
+    mensaje += "Subtotal: $" + subTotal + "\n"
+    mensaje += "IVA: $" + iva + "\n"
+    mensaje += "Total: $" + total
+
+    alert(mensaje)
+
   } else {
     alert("Te esperamos cuando gustes!!")
   }
 }
 
-saludarUsuario();
-mostrarProductos(productos);
-comprar(productos,carrito);
-mostrarCarrito(carrito);
 
-console.log(carrito);
+saludarUsuario();
+mostrarProductos(productos, precios);
+let datos = comprar(productos, carrito, precios)
+let subTotal = datos[0]
+let iva = datos[1]
+let totalFinal = datos[2]
+mostrarCarrito(carrito, carritoPrecios, totalFinal, iva, subTotal);
+console.log("Carrito", carrito);
+console.log("Precios", carritoPrecios);
+console.log("Total", totalFinal);
